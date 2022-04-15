11b93f1663a93a83b795b458f38ff9f83265b64d
c240615 <97713225+c240615@users.noreply.github.com>
2022-04-15 15:14:40 +0800

    Add files via upload

---
+++ HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+     <p>歡迎來到{{msg}}的專案</p>
+     <button @click="getin">進入todolist</button>
+     
+  </div> 
+</template>
+// import demo from '路徑'
+<script>
+export default {
+  name: 'HelloVue',
+  data () {
+    return {
+      msg: '周培瑜',
+      getin(){
+        window.open('')
+      }
+    }  
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
